170b2c126b70dfc40fa0771e397c3a6a55d5b7ec
nanjiangshu <nanjiang.shu@gmail.com>
2019-12-16 13:36:13 +0100

    add plotting scripts to setup

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+import glob
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+plotting_scripts = glob.glob('src/plot*.sh')
 
 setup(
         name='libpredweb',
         version='1.0.0',
         scripts=['src/clean_cached_result.py', 'src/clean_server_file.sh',
-            'src/nanjianglib.pl', 'src/check_web_server.pl'] ,
+            'src/nanjianglib.pl', 'src/check_web_server.pl']+plotting_scripts ,
         author="Nanjiang Shu",
         author_email="nanjiang.shu@gmail.com",
         description="A library for the prediction protein web-server",
