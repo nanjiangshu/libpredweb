a7be4f327385f4dbcd0a0a641ffb50cb3331627b
nanjiangshu <nanjiang.shu@gmail.com>
2021-03-04 21:12:03 +0100

    catch exception when date_diff failed

--- libpredweb/myfunc.py
+++ libpredweb/myfunc.py
@@ -1768,36 +1768,39 @@ def date_diff(older, newer):#{{{
     0 days 4 hours 3 minutes returns '4 hours', etc...
     """
 
-    timeDiff = newer - older
-    days = timeDiff.days
-    hours   = int(timeDiff.seconds/3600+0.5)
-    minutes = int(timeDiff.seconds%3600/60+0.5)
-    seconds = timeDiff.seconds%3600%60
+    try: 
+        timeDiff = newer - older
+        days = timeDiff.days
+        hours   = int(timeDiff.seconds/3600+0.5)
+        minutes = int(timeDiff.seconds%3600/60+0.5)
+        seconds = timeDiff.seconds%3600%60
 
-    str = ""
-    tStr = ""
-    if days > 0:
-        if days == 1:   tStr = "day"
-        else:           tStr = "days"
-        str = str + "%s %s" %(days, tStr)
-        return str
-    elif hours > 0:
-        if hours == 1:  tStr = "hour"
-        else:           tStr = "hours"
-        str = str + "%s %s" %(hours, tStr)
-        return str
-    elif minutes > 0:
-        if minutes == 1:tStr = "min"
-        else:           tStr = "mins"
-        str = str + "%s %s" %(minutes, tStr)
-        return str
-    elif seconds >= 0:
-        if seconds <= 1:tStr = "sec"
-        else:           tStr = "secs"
-        str = str + "%s %s" %(seconds, tStr)
-        return str
-    else:
-        return None
+        str = ""
+        tStr = ""
+        if days > 0:
+            if days == 1:   tStr = "day"
+            else:           tStr = "days"
+            str = str + "%s %s" %(days, tStr)
+            return str
+        elif hours > 0:
+            if hours == 1:  tStr = "hour"
+            else:           tStr = "hours"
+            str = str + "%s %s" %(hours, tStr)
+            return str
+        elif minutes > 0:
+            if minutes == 1:tStr = "min"
+            else:           tStr = "mins"
+            str = str + "%s %s" %(minutes, tStr)
+            return str
+        elif seconds >= 0:
+            if seconds <= 1:tStr = "sec"
+            else:           tStr = "secs"
+            str = str + "%s %s" %(seconds, tStr)
+            return str
+        else:
+            return ""
+    except:
+        return ""
 #}}}
 def second_to_human(time_in_sec):#{{{
     """
